ad7e8e85ebd4b42aebdf0e6e4c2a8510f1d734d3
Ryan Anderson <ryan@joshandryan.net>
2023-12-08 02:24:33 -0600

    Add configuration loading and logging setup

--- assistant.py
+++ assistant.py
@@ -7,9 +7,29 @@ import discord
 from discord.ext import commands
 import openai
 
-# Load config
+# Load configuration from YAML file
+def load_config(config_path):
+    with open(config_path, 'r') as file:
+        return yaml.safe_load(file)
 
-# Set up logging
+config = load_config('config.yaml')
+
+# Configure logging based on settings from config
+logging.basicConfig(level=config['logging']['level'], 
+                    format=config['logging']['format'])
+
+# Initialize the Discord bot with the specified command prefix and intents
+intents = discord.Intents.default()
+intents.guilds = config['bot']['intents']['guilds']
+intents.guild_messages = config['bot']['intents']['guild_messages']
+intents.dm_messages = config['bot']['intents']['dm_messages']
+intents.message_content = config['bot']['intents']['message_content']
+
+bot = commands.Bot(command_prefix=config['bot']['command_prefix'], intents=intents)
+
+if __name__ == '__main__':
+    bot.run(config['bot']['token'])
+
 
 # Set up database
 
